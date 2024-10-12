505d765d688438e3ad0a546d9bac161ce292fcbe
synicalmx <70176240+SynicalMX@users.noreply.github.com>
2024-10-11 23:14:45 -0400

    feat: simple homepage

--- src/lib/server/routes/_app.ts
+++ src/lib/server/routes/_app.ts
@@ -1,15 +1,28 @@
 import { router, publicProcedure, authedProcedure } from '../trpc';
 import { z } from 'zod';
 
+let quoteTimestamp: number = 0;
+let todaysQuote: string = "";
+
 export const appRouter = router({
 	greeting: publicProcedure
-		.input(
-			z.object({
-				name: z.string().optional(),
-			}),
-		)
 		.query(({ input }) => {
-			return `Welcome to ${input.name ?? 'the world'}!`;
+			let now = Date.now();
+			// if (now > (quoteTimestamp + 8.64e7)) {
+				quoteTimestamp = now;
+				fetch("https://gist.githubusercontent.com/robatron/a66acc0eed3835119817/raw/77493d3ddf69fbd9d69997e22e1a7c6c70c8bdf2/quotes.txt").then((res) => {
+					if (!res.ok) {
+						throw Error("Failed to fetch quotes!");
+					}
+					
+					let quoteBlock = res.text.toString()
+					let quotes = quoteBlock.split("\n");
+					let quoteIdx = Math.random() * (quotes.length - 0) + 0;
+					todaysQuote = quotes[quoteIdx] as string;
+				});
+				
+			//}
+			return todaysQuote;
 		}),
 	me: publicProcedure.query(({ ctx }) => {
 		return ctx.user;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,84 +1,92 @@
 <script lang="ts">
-	import NextStep from '$lib/components/NextStep.svelte';
+	import Card from '$lib/components/Card.svelte';
 	import { trpc } from '$lib/trpc';
 
-	const greeting = trpc.greeting.query({ name: 'the o7 stack' });
+	// const quote = trpc.greeting.query();
 </script>
 
-<main class="flex h-screen flex-col items-center justify-center">
-	<img src="/favicon.png" class="w-32" alt="o7 Logo" />
-	<!--
-		Notice how there's no flash of `undefined` here: that's because of the
-		SSR in `+page.server.ts`! Try changing the `name` to see the difference.
-	-->
-	<h1 class="text-3xl font-bold">{$greeting.data}</h1>
-	<h2 class="my-6 text-2xl">Next Steps:</h2>
+<main class="flex h-screen flex-col items-center justify-center space-y-5">
+	<!-- svelte-ignore a11y-img-redundant-alt -->
+	<img
+		src="favicon.png"
+		alt="A picture of Gengar from Pokémon"
+		class="outline outline-4 outline-violet-800 rounded-full drop-shadow-xl"
+	>
+
+	<!-- Daily Quote -->
+	<!-- <h1 class="">{$quote.data}</h1> -->
+
+	<!-- The Internet Plays Minecraft -->
 	<div class="flex max-w-5xl justify-center gap-4 px-3">
-		<NextStep
-			title="Edit this page"
-			learnMore="https://svelte.dev/tutorial/basics"
+		<Card
+			title="The Internet Plays Minecraft"
+			link="/minecraft"
 		>
 			<p>
-				Edit <code class="text-lime-300">src/routes/+page.svelte</code> to see your
-				changes live.
+				The <span class="text-violet-500">entire internet</span> shares a Minecraft player!
 			</p>
 			<p>
-				The source for these cards is in <code class="text-lime-300"
-					>src/lib/components/NextStep.svelte</code
-				>.
+				Play inside of you're browser, <span class="text-lime-300">no Minecraft account required!</span>
 			</p>
 			<p>
-				There's some global styling in <code class="text-lime-300"
-					>src/app.css</code
-				>.
+				Complete up to <span class="text-violet-500">10 actions a day</span> per user. <span class="text-lime-300">Good luck!</span>
 			</p>
-		</NextStep>
-		<NextStep
-			title="Work on your database schema"
-			learnMore="https://www.prisma.io/docs/concepts/components/prisma-schema"
-		>
-			<p>
-				Write a Prisma schema in <code class="text-red-300"
-					>prisma/schema.prisma</code
-				>.
-			</p>
-			<p>
-				Insert your database connection details into <code class="text-red-300"
-					>.env</code
-				>.
-			</p>
-			<p>
-				Run <code class="text-red-300">pnpm db:push</code> to update your database
-				typings.
-			</p>
-			<p>
-				Remember that you're using <a
-					href="https://github.com/kysely-org/kysely"
+		</Card>
+
+		<!-- Portfolio -->
+		<section class="flex w-1/3 flex-col gap-2 rounded-lg bg-zinc-800 p-4 shadow-lg items-center">
+			<h1 class="mb-2 text-xl font-bold text-center">hi. im <span class="text-violet-500">synical</span></h1>
+			<p class="text-center">i code stuff, enjoyer of pokémon, dragon ball, and an amazingly average video gamer</p>
+			<br>
+			<p class="text-center">follow my socials below</p>
+			<br>
+
+			<div class="flex flex-row h-full items-center space-x-3">
+				<!-- GitHub -->
+				<a
+					href="https://github.com/SynicalMX"
 					target="_blank"
-					class="text-blue-400 hover:underline">Kysely</a
+					class="size-8 fill-slate-200 hover:fill-violet-400"
 				>
-				instead of Prisma to make your queries, and
-				<code class="text-red-300">import {'{ db }'} from '$lib/db'</code>!
-			</p>
-		</NextStep>
-		<NextStep title="Create some tRPC routes" learnMore="https://trpc.io">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+				</a>
+
+				<!-- X -->
+				<a
+					href="https://x.com/SynicalMX"
+					target="_blank"
+					class="size-8 fill-slate-200 hover:fill-violet-400"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+				</a>
+
+				<!-- Discord -->
+				<a
+					href="https://discord.com/users/319948810505682944"
+					target="_blank"
+					class="size-8 fill-slate-200 mt-2 hover:fill-violet-400"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/></svg>
+				</a>
+
+				<!-- YouTube -->
+				<a
+					href="https://youtube.com/@synicalmx"
+					target="_blank"
+					class="size-8 fill-slate-200 mt-1 hover:fill-violet-400"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
+				</a>
+			</div>
+
+		</section>
+		<Card title="future project..." link="https://x.com/SynicalMX">
 			<p>
-				There's an example query in <code class="text-purple-300"
-					>src/lib/server/routes/_app.ts</code
-				>.
+				A space for the future.
 			</p>
 			<p>
-				Also take a look at <code class="text-purple-300">
-					src/routes/+page.server.ts</code
-				> to see how server-side rendering works!
+				In the mean time, go play "The Internet Plays Minecraft" 😉
 			</p>
-		</NextStep>
+		</Card>
 	</div>
 </main>
-
-<style>
-	code {
-		background: theme('colors.zinc.900');
-		padding: theme('spacing[0.5]');
-	}
-</style>
