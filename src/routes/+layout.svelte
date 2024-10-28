fc27b6978eb11d63cbe5a0e9bea3d2a4f47e35a6
synicalmx <70176240+SynicalMX@users.noreply.github.com>
2024-10-28 05:17:52 -0400

    chore: add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { inject } from '@vercel/analytics'
 	import type { LayoutData } from './$types';
 	import { trpc } from '$lib/trpc';
 
 	export let data: LayoutData;
 
 	const queryClient = trpc.hydrateFromServer(data.trpc);
+	inject();
 </script>
 
 <QueryClientProvider client={queryClient}>
